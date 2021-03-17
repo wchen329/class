a0ae8f4ad5823431563dd75350763f74bfac0ff8
wchen329 <wchen329@wisc.edu>
2021-03-16 20:55:37 -0500

    remmove rd_addr from memory map

--- contrib/dma_loopback/hw/memory_map.sv
+++ contrib/dma_loopback/hw/memory_map.sv
@@ -73,7 +73,7 @@ module memory_map
    
 		       mmio_if.user mmio,
    
-   output logic [ADDR_WIDTH-1:0] rd_addr, wr_addr,
+   output logic [ADDR_WIDTH-1:0] wr_addr,
    output logic [SIZE_WIDTH-1:0] size,
    output logic        go,
    input logic 	       done   
@@ -85,7 +85,6 @@ module memory_map
    always_ff @(posedge clk or posedge rst) begin 
       if (rst) begin
 	 go       <= '0;
-	 rd_addr  <= '0;
 	 wr_addr  <= '0;	     
 	 size     <= '0;
       end
@@ -107,25 +106,7 @@ module memory_map
    // MMIO read code
    // ============================================================= 	    
    always_ff @(posedge clk or posedge rst) begin
-      if (rst) begin
 	 mmio.rd_data <= '0;
-      end
-      else begin             
-         if (mmio.rd_en == 1'b1) begin
-	    
-	    mmio.rd_data <= '0;
-	    
-            case (mmio.rd_addr)
-
-	      16'h0052: mmio.rd_data[$size(rd_addr)-1:0] <= rd_addr;
-	      16'h0054: mmio.rd_data[$size(wr_addr)-1:0] <= wr_addr;
-	      16'h0056: mmio.rd_data[$size(size)-1:0] <= size;     	     
-	      16'h0058: mmio.rd_data[0] <= done;
-	      
-	      // If the processor requests an address that is unused, return 0.
-              default:  mmio.rd_data <= 64'h0;
-            endcase
-         end
-      end
+         // Here for legacy reasons might remove for optimization
    end
 endmodule
